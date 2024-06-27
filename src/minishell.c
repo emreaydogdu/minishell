5ae4eb3ab56156e72b6fa977ed385e8641acf17b
emreaydogdu <emre.aydogdu@outlook.de>
2024-06-27 15:06:37 +0200

    add loop and frontend

--- src/minishell.c
+++ src/minishell.c
@@ -6,13 +6,40 @@
 /*   By: emaydogd <emaydogd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 15:02:35 by emaydogd          #+#    #+#             */
-/*   Updated: 2024/06/25 15:02:35 by emaydogd         ###   ########.fr       */
+/*   Updated: 2024/06/27 15:00:12 by emaydogd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <printf.h>
 #include "minishell.h"
-#include "unistd.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
-	printf("%s", BANNER);
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+void minishell()
+{
+	char *line = NULL;
+	size_t len = 0;
+
+	while (1) {
+		printf("\033[36;1m%s\033[0m", "➜ minishell$ ");
+		if (getline(&line, &len, stdin) == -1)
+			break;
+		printf("%s", line);
+		line[ft_strlen(line)-1] = '\0';
+		// ... parse & execute with var line
+	}
+	free(line);
+}
+
+int main() {
+	minishell();
+	return 0;
 }
