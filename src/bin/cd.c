3e1cf4c7b78770e441521fea8001ed82bc706fbc
Chakib Bachir <chakibbachir88@gmail.com>
2024-09-23 09:10:19 +0200

    fix small issue in cd

--- src/bin/cd.c
+++ src/bin/cd.c
@@ -6,7 +6,7 @@
 /*   By: chbachir <chbachir@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 21:27:23 by emaydogd          #+#    #+#             */
-/*   Updated: 2024/09/20 18:41:03 by chbachir         ###   ########.fr       */
+/*   Updated: 2024/09/20 23:27:44 by chbachir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 void	exec_cd(t_shell *shell)
 {
 	t_parser *parser;
+	char	*home;
 	
 	parser = shell->parser;
-	
-	char	*home;
-
 	home = getenv("HOME");
-	if ((char *)shell->parser->args == NULL || ft_strncmp((char *)shell->parser->args, "~", 1) == 0)
+	if ((char *)shell->parser->args->content == NULL || ft_strncmp((char *)shell->parser->args->content, "~", 1) == 0)
 	{
 		if (chdir(home) == 0)
 			printf("%s\n", ".");
 	}
-	else if (chdir(shell->parser->args) == 0)
+	else if (chdir((char  *)shell->parser->args->content) == 0)
+	{
 		printf("%s\n", ".");
+	}
 }
