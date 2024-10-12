2bfba742c392051d5f4da90a4ff5cba4087f8def
Chakib Bachir <chakibbachir88@gmail.com>
2024-10-12 21:04:02 +0200

    fixed cd

--- src/bin/cd.c
+++ src/bin/cd.c
@@ -6,7 +6,7 @@
 /*   By: chbachir <chbachir@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 21:27:23 by emaydogd          #+#    #+#             */
-/*   Updated: 2024/09/23 12:52:42 by chbachir         ###   ########.fr       */
+/*   Updated: 2024/10/12 21:01:26 by chbachir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,30 @@ void	exec_cd(t_shell *shell)
 	char	*home;
 	
 	parser = shell->parser;
-	home = getenv("HOME");
-	if ((char *)shell->parser->args == NULL || ft_strncmp((char *)shell->parser->args->content, "~", 1) == 0)
+	home = ft_getenv(shell,"HOME");
+	if (home == NULL)
 	{
-		if (chdir(home)== 0)
-			error(shell, NULL, NULL);
-		else
-			error(shell, "cd: %s: No such file or directory\n", home);
+		error(shell, "bash: cd: HOME not set\n", NULL);
+		return ;
 	}
-	else if (chdir((char  *)shell->parser->args->content) == 0)
+	if (ft_lstsize(parser->args) >= 2)
+	{
+		error(shell, "bash: cd: too many arguments\n", NULL);
+		return ;
+	}
+	if (parser->args == NULL || parser->args->content == NULL || ft_strncmp((char *)parser->args->content, "~", 1) == 0)
+	{
+    	if (chdir(home) == 0)
+        	error(shell, NULL, NULL);
+    	else
+        	error(shell, "bash: cd: %s: No such file or directory\n", home);
+    	return;
+	}
+	else if (chdir((char  *)parser->args->content) == 0)
 		error(shell, NULL, NULL);
 	else
-		error(shell, "cd: %s: No such file or directory\n", (char *)shell->parser->args->content);
+		error(shell, "bash: cd: %s: No such file or directory\n", (char *)parser->args->content);
 }
+
+
+
